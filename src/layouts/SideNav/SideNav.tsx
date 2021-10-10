ad867c72229c936ac22d9fc3e4373f523b3f440e
realmiya <xiaoyu.dongmiyamiya@gmail.com>
2021-10-11 05:20:16 +1100

    test

---
+++ src/layouts/SideNav/SideNav.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import styles from "./SideNav.module.scss";
+import SideListDiv from "./components/Sidelistdiv";
+
+interface sideProps {
+    unfolded: boolean;
+    collapse: () => void;
+}
+
+const Sidebar: React.FC<sideProps> = ({ unfolded, collapse }: sideProps) => (
+    <>
+        <nav
+            className={unfolded ? styles.sidebar : styles["sidebar--collapsed"]}
+        >
+            <div
+                className={
+                    unfolded
+                        ? styles.sideWrapper
+                        : styles["sideWrapper--collapsed"]
+                }
+            >
+                <div
+                    className={
+                        unfolded
+                            ? styles.sideWrapper__navBtn
+                            : styles["sideWrapper__navBtn--collapsed"]
+                    }
+                >
+                    {unfolded && (
+                        <div
+                            className={styles.sideWrapper__navBtn__arrowBtn}
+                            onClick={() => {
+                                collapse();
+                            }}
+                        >
+                            <FiChevronLeft />
+                            Hide
+                        </div>
+                    )}
+                    {!unfolded && (
+                        <div
+                            className={styles.sideWrapper__navBtn__arrowBtn}
+                            onClick={() => {
+                                collapse();
+                            }}
+                        >
+                            <FiChevronRight />
+                            Show
+                        </div>
+                    )}
+                </div>
+                <ul
+                    className={
+                        unfolded
+                            ? styles.sideMenu
+                            : styles["sideMenu--collapsed"]
+                    }
+                >
+                    <SideListDiv unfolded={unfolded} />
+                </ul>
+            </div>
+        </nav>
+    </>
+);
+export default Sidebar;
